ba0da642c702757f63a670317ab90d7898760dfc
ngalan3 <manlokalan.ng@mail.utoronto.ca>
2018-11-25 15:01:58 -0500

    - Update algorithms in BoardManager.java to fit Matching Cards game

--- Phase 2/app/src/main/java/fall2018/csc2017/GameCentre/MatchingCards/BoardManager.java
+++ Phase 2/app/src/main/java/fall2018/csc2017/GameCentre/MatchingCards/BoardManager.java
@@ -1,5 +1,7 @@
 package fall2018.csc2017.GameCentre.MatchingCards;
 
+import android.util.Pair;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -7,6 +9,7 @@ import java.util.List;
 import java.util.Stack;
 
 //import fall2018.csc2017.GameCentre.SlidingTiles.Board;
+import fall2018.csc2017.GameCentre.R;
 import fall2018.csc2017.GameCentre.Tile;
 
 import static fall2018.csc2017.GameCentre.MatchingCards.Board.NUM_COLS;
@@ -19,10 +22,18 @@ public class BoardManager extends fall2018.csc2017.GameCentre.BoardManager {
 
     private Board board;
 
-    /**
-     * A stack that keeps track of completed moves.
-     */
-    private Stack<int[][]> undoTrack = new Stack<>();
+    private int pairID = 0;
+
+    private Stack<int[]> clearedCards;
+
+    private Pair<Integer, Integer> lastClick;
+
+    private int[] cardIDs = {
+            R.drawable.tile_1, R.drawable.tile_2,
+            R.drawable.tile_3, R.drawable.tile_4,
+            R.drawable.tile_5, R.drawable.tile_6,
+            R.drawable.tile_7, R.drawable.tile_8
+    };
 
     /**
      * Manage a board that has been pre-populated.
@@ -38,8 +49,14 @@ public class BoardManager extends fall2018.csc2017.GameCentre.BoardManager {
     public BoardManager() {
         List<Tile> tiles = new ArrayList<>();
         final int numTiles = NUM_ROWS * NUM_COLS;
+        int[] temp = new int[numTiles];
         for (int tileNum = 0; tileNum != numTiles; tileNum++) {
-            tiles.add(new Tile(tileNum));
+            if (tileNum % 2 == 0){
+                temp[tileNum] = cardIDs[tileNum];
+            } else {
+                temp[tileNum] = cardIDs[tileNum - 1];
+            }
+            tiles.add(new Tile(tileNum + 1, temp[tileNum]));
         }
         createBoard(tiles);
     }
@@ -55,7 +72,7 @@ public class BoardManager extends fall2018.csc2017.GameCentre.BoardManager {
     @Override
     protected void createBoard(List<Tile> tiles) {
         // board = null;
-        while (board == null || !isSolvable(board)) {
+        while (board == null) {
             Collections.shuffle(tiles);
             board = new Board(tiles);
         }
@@ -69,14 +86,8 @@ public class BoardManager extends fall2018.csc2017.GameCentre.BoardManager {
     @Override
     public boolean puzzleSolved() {
         boolean solved = true;
-        Iterator<Tile> iter = board.iterator();
-        int expectedId = 1;
-        while (iter.hasNext()) {
-            if (expectedId != iter.next().getId()) {
-                solved = false;
-                break;
-            }
-            expectedId++;
+        if (clearedCards.size() != board.numTiles()) {
+            solved = false;
         }
         if (solved) {
             updateScore();
@@ -95,16 +106,11 @@ public class BoardManager extends fall2018.csc2017.GameCentre.BoardManager {
 
         int row = position / NUM_COLS;
         int col = position % NUM_COLS;
-        int blankId = board.numTiles();
-        // Are any of the 4 the blank tile?
-        Tile above = row == 0 ? null : board.getTile(row - 1, col);
-        Tile below = row == NUM_ROWS - 1 ? null : board.getTile(row + 1, col);
-        Tile left = col == 0 ? null : board.getTile(row, col - 1);
-        Tile right = col == NUM_COLS - 1 ? null : board.getTile(row, col + 1);
-        return (below != null && below.getId() == blankId)
-                || (above != null && above.getId() == blankId)
-                || (left != null && left.getId() == blankId)
-                || (right != null && right.getId() == blankId);
+        if (!clearedCards.empty()) {
+            int[] clickedOn = {row, col};
+            return (clearedCards.search(clickedOn) == -1);
+        }
+        return true;
     }
 
     /**
@@ -117,93 +123,26 @@ public class BoardManager extends fall2018.csc2017.GameCentre.BoardManager {
         //1D representation of a 2D Cell
         int row = position / NUM_COLS;
         int col = position % NUM_COLS;
-        int blankId = board.numTiles(); // numRows * NUM_COLS per row + extra
+        int currentID = board.getTile(row, col).getId();
 
-        // tiles is the blank tile, swap by calling Board's swap method.\int[] dx = {0, 0, -1, 1};
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {-1, 1, 0, 0};
-        for (int i = 0; i < 4; i++) {
-            int row1 = dx[i] + row;
-            int col1 = dy[i] + col;
-            if (row1 >= 0 && col1 >= 0 && row1 < NUM_ROWS && col1 < NUM_COLS) {
-                if (board.getTile(row1, col1).getId() == blankId) {
-                    int[][] lastStep = {{row1, col1}, {row, col}};
-                    undoTrack.push(lastStep);
-                    board.swapTiles(row, col, row1, col1);
-
-                    score--;
-
-                }
-            }
-        }
-    }
-
-    /**
-     * Undo a move up to unlimited times until the board is as original.
-     */
-    public void undoMove(){
-        if(!undoTrack.isEmpty()) {
-            int[][] lastStep = undoTrack.pop();
-
-            board.swapTiles(lastStep[0][0], lastStep[0][1], lastStep[1][0], lastStep[1][1]);
-            score++;
-        }
-    }
-
-    /**
-     *
-     * @param board
-     * @return whether or not the board is solvable
-     */
-    boolean isSolvable(Board board) {
-        int invCount = getInvCount(board);
-        if (NUM_ROWS % 2 == 1) {
-            return (invCount % 2 == 0);
-        } else {
-            int blankTile = findReverseRowOfBlankTile(board);
-            if (blankTile % 2 == 1) {
-                return (invCount % 2 == 0);
+        if (pairID == 0) {
+            int[] clickedOn = {row, col};
+            clearedCards.push(clickedOn);
+            if (currentID % 2 == 0) {
+                pairID = currentID - 1;
             } else {
-                return (invCount % 2 == 1);
+                pairID = currentID + 1;
             }
-        }
-    }
-
-    private int findReverseRowOfBlankTile(Board board) {
-        for (int i = NUM_ROWS - 1; i >= 0; i--) {
-            for (int j = NUM_ROWS - 1; j >= 0; j--) {
-                if (board.getTile(i,j).getId() == board.numTiles()) {
-                    return NUM_ROWS - i;
-                }
-            }
-        }
-        return -1;
-    }
-
-    /**
-     * Counts inversions to determine if board is solvable
-     * @param board
-     * @return number of inversions
-     */
-    private int getInvCount(Board board) {
-        List<Tile> list = new ArrayList<>();
-        for (int i = 0; i < NUM_COLS; i++) {
-            for (int j = 0; j < NUM_ROWS; j++) {
-                Tile tile = board.getTile(i,j);
-                if (!tile.isBlank()) {
-                    list.add(tile);
-                }
+        } else {
+            if (pairID == currentID) {
+                int[] clickedOn = {row, col};
+                clearedCards.push(clickedOn);
+                pairID = 0;
+            } else {
+                clearedCards.pop();
+                pairID = 0;
             }
         }
 
-        int count = 0;
-        for (int i = 0; i < list.size() - 1; i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                if (list.get(i).getId() > list.get(j).getId()) {
-                    count++;
-                }
-            }
-        }
-        return count;
     }
 }
