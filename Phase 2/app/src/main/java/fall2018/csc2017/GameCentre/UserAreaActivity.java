e4db151630afab52f74abc18cb8c88e0747700af
ohchae <lisa.oh@mail.utoronto.ca>
2018-11-30 01:18:31 -0500

    Added javadoc for entire class

--- Phase 2/app/src/main/java/fall2018/csc2017/GameCentre/UserAreaActivity.java
+++ Phase 2/app/src/main/java/fall2018/csc2017/GameCentre/UserAreaActivity.java
@@ -17,6 +17,10 @@ public class UserAreaActivity extends AppCompatActivity {
      * The account manager.
      */
     private AccountManager accountManager;
+
+    /**
+     * The board manager.
+     */
     private BoardManager boardManager;
 
     @Override
@@ -36,6 +40,9 @@ public class UserAreaActivity extends AppCompatActivity {
         textView.setText("Welcome " + accountManager.getCurrentAccount().getName() + ",");
     }
 
+    /**
+     * Activate the Sliding Tiles game button.
+     */
     private void addSlidingTilesButtonListener() {
         Button SlidingTilesButton = findViewById(R.id.SlidingTilesButton);
         SlidingTilesButton.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +55,10 @@ public class UserAreaActivity extends AppCompatActivity {
             }
         });
     }
+
+    /**
+     * Activate the Matching Cards game button.
+     */
     private void addMatchingCardsButtonListener() {
         Button MatchingCardsButton = findViewById(R.id.MatchingCardsButton);
         MatchingCardsButton.setOnClickListener(new View.OnClickListener() {
@@ -60,7 +71,11 @@ public class UserAreaActivity extends AppCompatActivity {
             }
         });
     }
-   private void addSudokuButtonListener() {
+
+    /**
+     * Activate the Sudoku game button.
+     */
+    private void addSudokuButtonListener() {
         Button SudokuButton = findViewById(R.id.SudokuButton);
         SudokuButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,6 +86,10 @@ public class UserAreaActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Switch to StartingActivity.
+     * @param gameStartingActivity the game starting activity.
+     */
     private void switchToGame(Class gameStartingActivity) {
         LoadAndSave.saveToFile(LoadAndSave.ACCOUNT_MANAGER_FILENAME, accountManager, this);
         // save boardManager so that we know which game is being played
@@ -81,6 +100,10 @@ public class UserAreaActivity extends AppCompatActivity {
         startActivity(tmp);
     }
 
+    /**
+     * Switch to StartingActivity.
+     * @param gameStartingActivity the game starting activity.
+     */
     private void switchToSudoku(Class gameStartingActivity) {
         LoadAndSave.saveToFile(LoadAndSave.ACCOUNT_MANAGER_FILENAME, accountManager, this);
         // save boardManager so that we know which game is being played
